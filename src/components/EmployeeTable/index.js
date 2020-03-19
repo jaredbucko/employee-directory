c1028680abbb3ca51714a824611327ad6d4d0de8
jaredbucko <jaredbucko@icloud.com>
2020-03-18 21:20:20 -0700

    update EmployeeTable jsx

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -6,34 +6,32 @@ const EmployeeTable = () => {
   const persons = useContext(PersonsContext);
   console.log(persons);
   return (
-    <div>
-      <table className="table table-striped">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Photo</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Email</th>
-            <th scope="col">DOB</th>
-          </tr>
-        </thead>
-        <tbody>
-          { persons.map((person, index) => {
-            return (
-              <tr key={index}>
-                <td className="align-middle"><img src={person.picture.large} alt="headshot"></img></td>
-                <td className="align-middle">{person.name.last}</td>
-                <td className="align-middle">{person.name.first}</td>
-                <td className="align-middle">{person.phone}</td>
-                <td className="align-middle">{person.email}</td>
-                <td className="align-middle">{person.dob.date}</td>
-              </tr>
-              );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <table className="table table-striped">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">Photo</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Email</th>
+          <th scope="col">DOB</th>
+        </tr>
+      </thead>
+      <tbody>
+        { persons.map((person, index) => {
+          return (
+            <tr key={index}>
+              <td className="align-middle"><img src={person.picture.large} alt="headshot"></img></td>
+              <td className="align-middle">{person.name.last}</td>
+              <td className="align-middle">{person.name.first}</td>
+              <td className="align-middle">{person.phone}</td>
+              <td className="align-middle">{person.email}</td>
+              <td className="align-middle">{person.dob.date}</td>
+            </tr>
+            );
+        })}
+      </tbody>
+    </table>
   );
 }
 
