da016bec0812b36e9d620dd0603d2404499932de
jaredbucko <jaredbucko@icloud.com>
2020-03-18 19:55:50 -0700

    created personsContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
+import axios from "axios";
+import PersonsContext from "./components/PersonsContext/personsContext";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import JumboTron from "./components/JumboTron";
 import SearchBar from "./components/SearchBar";
@@ -6,14 +8,26 @@ import Employees from "./pages/Employees";
 import StaticEmployees from "./pages/StaticEmployees";
 
 function App() {
+  const [personsState, setPersonsState] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/api/?results=200&nat=us`)
+    .then(res => {
+      const personsArr = res.data.results;
+      setPersonsState(personsArr);
+    })
+  })
+
   return (
     <Router>
-      <div>
-        <JumboTron />
-        <SearchBar />
-        <Route exact path="/" component={Employees} />
-        <Route exact path="/static" component={StaticEmployees} />
-      </div>
+      <PersonsContext.Provider value={personsState}>
+        <div>
+          <JumboTron />
+          <SearchBar />
+          <Route exact path="/" component={Employees} />
+          <Route exact path="/static" component={StaticEmployees} />
+        </div>
+      </PersonsContext.Provider>
     </Router>
   );
 }
--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -1,30 +1,36 @@
-import React from "react";
-import EmployeeRow from "../EmployeeRow";
+import React, { useContext } from "react";
 import "./style.css";
+import PersonsContext from "../PersonsContext/personsContext"
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function EmployeeTable(props) {
+const EmployeeTable = () => {
+  const persons = useContext(PersonsContext);
+  console.log(persons.length);
   return (
     <table className="table table-striped">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col">Photo</th>
-              <th scope="col">Name</th>
-              <th scope="col">Phone</th>
-              <th scope="col">Email</th>
-              <th scope="col">DOB</th>
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">Photo</th>
+          <th scope="col">Name</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Email</th>
+          <th scope="col">DOB</th>
+        </tr>
+      </thead>
+      <tbody>
+        { persons.map((person, index) => {
+          return (
+            <tr key={index}>
+              <td className="align-middle"><img src={person.photo} alt="headshot"></img></td>
+              <td className="align-middle">{person.name}</td>
+              <td className="align-middle">{person.phone}</td>
+              <td className="align-middle">{person.email}</td>
+              <td className="align-middle">{person.DOB}</td>
             </tr>
-          </thead>
-          <tbody>
-            <EmployeeRow
-              photo={props.photo}
-              name={props.name}
-              phone={props.phone}
-              email={props.email}
-              DOB={props.DOB}
-            />
-          </tbody>
-        </table>
+            );
+        })}
+      </tbody>
+    </table>
   );
 }
 
